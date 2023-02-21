d81f7be401ec8865cd8c818dc4ede7238892cee0
z9in <113665653+z9in@users.noreply.github.com>
2023-02-21 15:46:24 +0900

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
-let headerEl = document.getElementsByClassName('header')[0];
+let headerEl = document.getElementsByClassName("header")[0];
 
-headerEl.innerHTML=`
+headerEl.innerHTML = `
 <header>
 <h1><a href="./index.html"><img src="./images/logoB.svg" alt="logo"></a></h1>
 <nav>
@@ -8,24 +8,19 @@ headerEl.innerHTML=`
         <li><a href="./index.html">HOME</a></li>
         <li><a href="./about.htm">ABOUT</a></li>
         <li class="nav_works"><a href="./works.html">WORKS</a>
-            <ul class="depth2">
-                <li><a href="./works.html#individual">Individual</a></li>
-                <li><a href="./works.html#team">Team</a></li>
-            </ul>
         </li>
     </ul>
 </nav>   
 <img id="nav_open" src="./images/icon _menu_.svg" alt="menu">
-</header>`
+</header>`;
 
-let navEl = document.getElementsByTagName('nav')[0];
-let toggle = document.getElementById('nav_open')
+let navEl = document.getElementsByTagName("nav")[0];
+let toggle = document.getElementById("nav_open");
 
-toggle.addEventListener('click', ()=>{
-    if(navEl.style.display=='flex'){
-        navEl.style.display='none'
-    }else {
-        navEl.style.display='flex'
-    }
-    
-})
+toggle.addEventListener("click", () => {
+  if (navEl.style.display == "flex") {
+    navEl.style.display = "none";
+  } else {
+    navEl.style.display = "flex";
+  }
+});
