e01a58775981d1c537a2765e38f2f1becf0161f5
z9in <113665653+z9in@users.noreply.github.com>
2023-02-22 16:18:31 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,19 +1,3 @@
-let headerEl = document.getElementsByClassName("header")[0];
-
-headerEl.innerHTML = `
-<header>
-<h1><a href="./index.html"><img src="./images/logoB.svg" alt="logo"></a></h1>
-<nav>
-    <ul class="depth1">
-        <li><a href="./index.html">HOME</a></li>
-        <li><a href="./about.htm">ABOUT</a></li>
-        <li class="nav_works"><a href="./works.html">WORKS</a>
-        </li>
-    </ul>
-</nav>   
-<img id="nav_open" src="./images/icon _menu_.svg" alt="menu">
-</header>`;
-
 let navEl = document.getElementsByTagName("nav")[0];
 let toggle = document.getElementById("nav_open");
 
