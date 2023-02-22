f44350c83b1504e08dc0fbdc23a284c16b4b03c1
z9in <113665653+z9in@users.noreply.github.com>
2023-02-22 16:21:38 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -9,12 +9,3 @@ toggle.addEventListener("click", () => {
   }
 });
 
-let pdfDown = document.getElementById('pdf');
-
-pdfDown.addEventListener('click',(e)=>{
-    if (window.confirm("다운로드하시겠습니까?")) {
-  } else {
-    e.preventDefault();
-    window.location.reload();
-  }
-})
