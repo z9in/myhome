4b72d01784643e490354c0974e25f2e541e0de73
z9in <113665653+z9in@users.noreply.github.com>
2023-02-22 16:12:57 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -31,6 +31,6 @@ pdfDown.addEventListener('click',(e)=>{
     if (window.confirm("다운로드하시겠습니까?")) {
   } else {
     e.preventDefault();
-    window.location.reload("./index.html");
+    window.location.reload();
   }
 })
